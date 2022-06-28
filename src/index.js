13363ffee16dd782e7f009ded87cf6febf9dc924
OhByeongHeon <hi1004@github.com>
2022-06-28 16:35:17 +0900

    add GlobalStyles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'antd/dist/antd.min.css';
-import './styles/css/index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import GlobalStyle from './styles/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <BrowserRouter>
       <App />
     </BrowserRouter>
